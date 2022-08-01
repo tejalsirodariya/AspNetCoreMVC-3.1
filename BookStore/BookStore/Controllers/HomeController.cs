03270dd56276d43dd38cbc0bdf8986be5d22aee3
Tejal Sirodariya <tejalsirodariya@gmail.com>
2022-08-01 12:45:16 +0530

    Model, View and Controller added

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -4,9 +4,21 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "BookStore";
+            // var obj = new {Id = 1, Name = "Tejal"}; 
+            //return View("AboutUs", obj);
+            // return View("~/TempView/TejalTemp.cshtml");   // full path
+            //return View("../../TempView/TejalTemp");        //Relative path
+            return View();
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
+        {
+            return View();
         }
     }
 }
